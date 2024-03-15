fbe4aaa257bfb50c6a254ea252fd0e5aecc5ac6a
Vladimir Antonov <antonovvk@yandex.ru>
2024-03-15 13:25:59 +0300

    Experiments

---
+++ comm.py
@@ -0,0 +1,33 @@
+Import("env")
+import serial
+
+#~ port = env.GetProjectOption("comm_port")
+
+def comm_callback(*args, **kwargs):
+    ser = serial.Serial("COM12", rtscts=True, dsrdtr=True)
+    while True:
+        comm = input()
+        print("Sending command: {}".format(comm))
+        msg = "{}\r\n".format(comm)
+        ser.write(msg.encode())
+        ser.flush()
+        res = ser.read_until(expected="\r\n")
+        print("Received answer: {}".format(res))
+        #~ ser.close()
+
+
+env.AddCustomTarget("comm", None, comm_callback)
+
+
+def after_upload(source, target, env):
+    ser = serial.Serial("COM12", rtscts=True, dsrdtr=True)
+    #~ while True:
+        #~ comm = input()
+    comm = "M1009"
+    print("Sending command: {}".format(comm))
+    msg = "{}\r\n".format(comm)
+    ser.write(msg.encode())
+    ser.flush()
+    res = ser.read_until(expected="\r\n")
+    print("Received answer: {}".format(res))
+        #~ ser.close()
