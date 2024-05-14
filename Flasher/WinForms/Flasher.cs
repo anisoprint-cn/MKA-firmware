aa492726322a7bc0e1742a3e72c96ade568bcdce
Vladimir Antonov <antonovvk@yandex.ru>
2024-05-14 21:57:17 +0300

    WinForms implementation

---
+++ Flasher/WinForms/Flasher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Eto.Forms;
+using FlasherGUI;
+
+namespace FlasherWinForms
+{
+  public partial class FlasherForm : System.Windows.Forms.Form
+  {
+    public FlasherForm()
+    {
+      InitializeComponent();
+
+      // Get native control for the panel
+      // passing true so that we can embed, otherwise we just get a reference to the control
+      var nativeView = new FlasherPanel().ToNative(true);
+      // set where we want it, size, dock attributes, etc.
+      nativeView.Location = new Point(100, 100);
+      //nativeView.Dock = DockStyle.Fill;
+
+      Controls.Add(nativeView);
+    }
+  }
+}
